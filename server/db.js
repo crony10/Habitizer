4b4e46e0a0472af8c5a076499b9afcd322d86fa7
cronypaul <cronypaul@gmail.com>
2023-07-18 23:57:48 +0530

    added env changes

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
+require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.HOST,
+    user: `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    host: `${process.env.HOST}`,
     port: process.env.PORT,
-    database: process.env.DB_NAME,
+    database: `${process.env.DB_NAME}`,
     ssl: {
         rejectUnauthorized: false // Adjust this based on your SSL configuration
     }
