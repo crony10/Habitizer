08c909a830c2ee870300596761e2608ea8397639
cronypaul <cronypaul@gmail.com>
2023-07-19 00:10:38 +0530

    fixed a production bug

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     user: `${process.env.DB_USER}`,
     password: `${process.env.DB_PASSWORD}`,
     host: `${process.env.HOST}`,
-    port: process.env.PORT,
+    port: 5432,
     database: `${process.env.DB_NAME}`,
     ssl: {
         rejectUnauthorized: false // Adjust this based on your SSL configuration
