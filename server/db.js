82a9ff760cf5bc3e80fd539a5223e7bfa19ff25b
cronypaul <cronypaul@gmail.com>
2023-07-19 00:14:44 +0530

    reverted changes

--- server/db.js
+++ server/db.js
@@ -1,12 +1,11 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
 
 const pool = new Pool({
-    user: `${process.env.DB_USER}`,
-    password: `${process.env.DB_PASSWORD}`,
-    host: `${process.env.HOST}`,
+    user: "habitizerv1_user",
+    password: "nuoIqdjdtXi8Q0c2oPoUBGBGYBwZMGLZ",
+    host: "dpg-ch5bdfd269v8910giqq0-a.oregon-postgres.render.com",
     port: 5432,
-    database: `${process.env.DB_NAME}`,
+    database: "habitizerv1",
     ssl: {
         rejectUnauthorized: false // Adjust this based on your SSL configuration
     }
