ca9713fa87c4b9ccd510b05a293a35668b2cddf4
cronypaul <cronypaul@gmail.com>
2023-07-27 18:50:10 +0530

    added new db

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "habitizerv1_user",
-    password: "nuoIqdjdtXi8Q0c2oPoUBGBGYBwZMGLZ",
-    host: "dpg-ch5bdfd269v8910giqq0-a.oregon-postgres.render.com",
+    user: "habitizerdb_v2_user",
+    password: "HulTJbVAcQPujQxB7wHPegG9hpLLwb8U",
+    host: "dpg-cj16rii7l0ft7nkm1ql0-a.oregon-postgres.render.com",
     port: 5432,
-    database: "habitizerv1",
+    database: "habitizerdb_v2",
     ssl: {
         rejectUnauthorized: false // Adjust this based on your SSL configuration
     }
